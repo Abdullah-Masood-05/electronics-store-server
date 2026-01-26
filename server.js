fb2f343ee6614958ee13ddc56753a6b619e918be
Abdullah Masood <mabd051104@gmail.com>
2026-01-26 21:01:52 +0500

    Add database connection before starting the server

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+// import dotenv from "dotenv";
+// import app from "./app.js";
+
+// dotenv.config();
+
+// const PORT = process.env.PORT || 8000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
+
+// Connect database before starting server
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
