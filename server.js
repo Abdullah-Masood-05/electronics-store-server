8005a9b4fad3456cf8ac6e244203c26701e1c64a
Abdullah Masood <mabd051104@gmail.com>
2026-02-06 23:56:34 +0500

    Add Firebase service account credentials configuration file

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-// import dotenv from "dotenv";
-// import app from "./app.js";
-
-// dotenv.config();
-
-// const PORT = process.env.PORT || 8000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/db.js";
